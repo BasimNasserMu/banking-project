7e1b12561f91790bcb4b292b6d5c2d8dcfab228c
Basim Aldawood <431103991@s.mu.edu.sa>
2025-09-24 12:50:39 +0300

    Added open and save function of bank class

--- bank.py
+++ bank.py
@@ -1,27 +1,46 @@
 import csv
-from datetime import datetime
+from datetime import date, datetime
+import time
 
 
 class Transaction:
-    def __init__(self, transaction_type, amount, from_acc=None, to_acc=None):
+    def __init__(
+        self,
+        transaction_type,
+        amount,
+        from_acc=None,
+        to_acc=None,
+        timestamp=str(datetime.now()),
+    ):
         self.type = transaction_type
         self.amount = amount
         self.from_acc = from_acc
         self.to_acc = to_acc
-        self.timestamp = datetime.now()
+        self.timestamp = timestamp
 
 
 class Customer:
-    def __init__(self, account_id, frst_name, last_name, password, balance=0.0):
+    def __init__(
+        self,
+        account_id,
+        frst_name,
+        last_name,
+        password,
+        checking_balance=0.0,
+        savings_balance=0.0,
+        transaction_history=[],
+        overdraft_count=0,
+        is_active=True,
+    ):
         self.account_id = account_id
         self.frst_name = frst_name
         self.last_name = last_name
         self.__password = password  # make password private. reference: https://stackoverflow.com/questions/1641219/does-python-have-private-variables-in-classes
-        self.__checking_balance = balance
-        self.__savings_balance = 0.0
-        self.__transaction_history = []
-        self.overdraft_count = 0
-        self.is_active = True
+        self.__checking_balance = checking_balance
+        self.__savings_balance = savings_balance
+        self.__transaction_history = transaction_history
+        self.__overdraft_count = overdraft_count
+        self.is_active = is_active
 
     def is_authenticated(self, password):
         if password == self.__password:
@@ -32,7 +51,7 @@ class Customer:
     def check_status(self, password=None):
         if self.is_authenticated(password):
             if self.is_active:
-                if self.overdraft_count >= 2:
+                if self.__overdraft_count >= 2:
                     self.is_active = False
                     print(
                         "Account deactivated due to excessive overdrafts. pay the fees by depositing money or transferring money to reactivate"
@@ -86,7 +105,7 @@ class Customer:
                         f"Amount exceeds available balance. overdraft fee applied. Total amount: {amount + 35}"
                     )
                     amount += 35
-                    self.overdraft_count += 1
+                    self.__overdraft_count += 1
                 self.__checking_balance -= amount
                 self.__transaction_history.append(
                     Transaction("withdraw", amount, self.account_id, None)
@@ -104,7 +123,7 @@ class Customer:
                         f"Amount exceeds available balance. overdraft fee applied. Total amount: {amount + 35}"
                     )
                     amount += 35
-                    self.overdraft_count += 1
+                    self.__overdraft_count += 1
                 self.__checking_balance -= amount
                 to_customer.__checking_balance += amount
                 transaction = Transaction(
@@ -119,4 +138,77 @@ class Customer:
 
 
 class Bank:
-    pass
+    def __init__(self):
+        self.customers = []
+        self.transactions = []
+        self.current_customer = None
+        self.password = ""
+
+        with open("data/bank.csv", mode="r") as file:
+            reader = csv.reader(file)
+            for row in reader:
+                if row[0] != "account_id":
+                    self.customers.append(
+                        Customer(
+                            account_id=row[0],
+                            frst_name=row[1],
+                            last_name=row[2],
+                            password=row[3],
+                            checking_balance=float(row[4]),
+                            savings_balance=float(row[5]),
+                        )
+                    )
+        with open("data/transactions.csv", mode="r") as file:
+            reader = csv.reader(file)
+            for row in reader:
+                if row[0] != "type":
+                    self.transactions.append(
+                        Transaction(
+                            transaction_type=row[0],
+                            amount=float(row[1]),
+                            from_acc=row[2] if row[2] != "None" else None,
+                            to_acc=row[3] if row[3] != "None" else None,
+                            timestamp=row[4],
+                        )
+                    )
+
+    def save_data(self):
+        with open("data/bank.csv", mode="w", newline="") as file:
+            writer = csv.writer(file)
+            writer.writerow(
+                [
+                    "account_id",
+                    "frst_name",
+                    "last_name",
+                    "password",
+                    "checking_balance",
+                    "savings_balance",
+                ]
+            )
+            for customer in self.customers:
+                writer.writerow(
+                    [
+                        customer.account_id,
+                        customer.frst_name,
+                        customer.last_name,
+                        customer._Customer__password,
+                        customer._Customer__checking_balance,
+                        customer._Customer__savings_balance,
+                    ]
+                )
+        with open("data/transactions.csv", mode="w", newline="") as file:
+            writer = csv.writer(file)
+            writer.writerow(["type", "amount", "from_acc", "to_acc", "timestamp"])
+            for transaction in self.transactions:
+                writer.writerow(
+                    [
+                        transaction.type,
+                        transaction.amount,
+                        transaction.from_acc,
+                        transaction.to_acc,
+                        transaction.timestamp,
+                    ]
+                )
+
+
+bank = Bank()
