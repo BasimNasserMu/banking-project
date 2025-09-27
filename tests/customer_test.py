7cb5e40cfd3717ae9c655ef734153dc45211a2be
Basim Aldawood <431103991@s.mu.edu.sa>
2025-09-27 20:35:44 +0300

    Added more tests for withdraw

--- tests/customer_test.py
+++ tests/customer_test.py
@@ -7,6 +7,10 @@ class TestCustomer(TestCase):
         self.customer = Customer("000001", "Basim", "Nasser", "password123", 100.0)
         self.to_customer = Customer("000002", "Mohammed", "Fawaz", "password456", 50.0)
 
+    def test_accessing_private_password(self):
+        with self.assertRaises(AttributeError):
+            _ = self.customer.__password
+
     def test_deposit_with_correct_password(self):
         self.customer.deposit(50, password="password123")
         self.assertEqual(self.customer.get_balance(password="password123"), 150.0)
@@ -16,9 +20,24 @@ class TestCustomer(TestCase):
         self.assertFalse(result)
         self.assertEqual(self.customer.get_balance(password="password123"), 100.0)
 
-    def test_accessing_private_password(self):
-        with self.assertRaises(AttributeError):
-            _ = self.customer.__password
+    def test_deposit_negative_amount(self):
+        result = self.customer.deposit(-10, password="password123")
+        self.assertFalse(result)
+        self.assertEqual(self.customer.get_balance(password="password123"), 100.0)
+
+    def test_withdraw_with_correct_password(self):
+        self.customer.withdraw(50, password="password123")
+        self.assertEqual(self.customer.get_balance(password="password123"), 50.0)
+
+    def test_withdraw_with_incorrect_password(self):
+        result = self.customer.withdraw(50, password="wrongpassword")
+        self.assertFalse(result)
+        self.assertEqual(self.customer.get_balance(password="password123"), 100.0)
+
+    def test_withdraw_negative_amount(self):
+        result = self.customer.withdraw(-10, password="password123")
+        self.assertFalse(result)
+        self.assertEqual(self.customer.get_balance(password="password123"), 100.0)
 
     def test_transfer_with_correct_password_and_sufficient_balance(self):
         result = self.customer.transfer(40.0, self.to_customer, password="password123")
@@ -33,11 +52,6 @@ class TestCustomer(TestCase):
         self.assertFalse(result)
         self.assertEqual(self.customer.get_balance(password="password123"), 100.0)
         self.assertEqual(self.to_customer.get_balance(password="password456"), 50.0)
-
-    def test_deposit_negative_amount(self):
-        result = self.customer.deposit(-10, password="password123")
-        self.assertFalse(result)
-        self.assertEqual(self.customer.get_balance(password="password123"), 100.0)
 
     def test_transfer_negative_amount(self):
         result = self.customer.transfer(-20.0, self.to_customer, password="password123")
